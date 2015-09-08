41707330b42bf318aa939cb52df6133c81ac2629
chendind <364044816@qq.com>
2015-09-08 15:03:31 +0800

    更新

--- Public/js/public.js
+++ Public/js/public.js
@@ -1,213 +1,306 @@
 var sW = $(window).width(), sH = $(window).height(), _r = 0.68;
-$("#beyond").height(sH-sW*_r);
-$.each($("#emojibox .scrollbox .longbox"),function(){
-	var l = $(this).children('.emojitable').length;
-	$(this).css("width",l*100+"%").children('.emojitable').css("width",100/l+"%");
-})
-$("#uploadbox").bind('click',function(){
-	$('#photofile').trigger('click');
-})
-$("#photofile").on("change",function(){
-	var f = this.files[0];
-	if(!/image\/\w+/.test(f.type)){ 
-		alert("亲只能传图片哦"); 
-		return false; 
-	} 
-	var reader = new FileReader();
-	reader.readAsDataURL(f);
-	var img = new Image();
-	img.src = getUrl(f);
-	img.onload = function(){
-		var position = sizereset(sW,sW*_r,img.width,img.height);
-		this.height = position.h;
-		$(img).addClass('veryCenter');
-		$('.limitedbox').append($(img));
-	}
-})
-var tX, tY, oX, oY, imghtml;
-var isTouchStart = false;
-var hasEmojiOut = false;
-$(".emojitable div img").on('touchstart',function(e){
-	e.stopPropagation();
-	e.preventDefault();
-	tX = e.originalEvent.changedTouches[0].pageX;
-    tY = e.originalEvent.changedTouches[0].pageY;
-	oX = $(this).offset().left;
-	oY = $(this).offset().top;
-	var src = $(this).attr("src").replace("emoji64","emoji160");
-	imghtml = "<img src='"+src+"' class='emoji draging' style='left:"+oX+"px;top:"+oY+"px;transform:rotate({{rotate}}deg)' />";
-	// $(imghtml).appendTo('body').dragInit();
-	$(imghtml).appendTo('.limitedbox').dragInit();
-	isTouchStart = true;
-})
+	var position;// a global variable, which define imagebox's width, height
+	var canvas = document.getElementById("canvas"),
+	context = canvas.getContext('2d'),
+	// ratio = getPixelRatio(context);
+	ratio = 1;
+	$("body").height(sH).width(sW);
+	$.each($("#emojibox .scrollbox .longbox"),function(){
+		var l = $(this).children('.emojitable').length;
+		$(this).css("width",l*100+"%").children('.emojitable').css("width",100/l+"%");
+	})
+	$("#uploadbox").bind('touchend',function(){
+		$('#photofile').trigger('click');
+	})
+	$("#photofile").bind("change",function(){
+		loadingboxshow();
+		$(".imagebox").empty();
+		$("#ctrlbottom.ctrlbottom").removeClass('ctrlstate');
+		if(position != undefined){
+			context.clearRect(0,0,position.w*ratio,position.h*ratio);
+		}
+		var f = this.files[0];
+		if(!/image\/\w+/.test(f.type)){ 
+			alert("亲只能传图片哦"); 
+			return false; 
+		} 
+		var reader = new FileReader();
+		ImageOrientationFix({  
+            image:getUrl(f)
+            ,onFix:function(base64_str){  
+                var img = new Image();
+                img.src = base64_str;
+                img.onload = function(){
+					position = sizereset(sW,$(".innerbox").height(),this.width,this.height);
+					console.log(position.h);
+					this.height = position.h, this.width = position.w;
+					ratio = this.naturalHeight/this.height;
+					$('.innerbox .imagebox').css({
+						"width":position.w+"px",
+						"height":position.h+"px",
+						"left":position.dx+"px"
+					}).append($(this).addClass('userimage'));
+					$(".canvasbox").css({
+						"width":position.w+"px",
+						"height":position.h+"px",
+						"left":position.dx+"px"
+					});
+					canvas.style.width = position.w+"px";
+					canvas.style.height = position.h+"px";
+					canvas.setAttribute("width",position.w*ratio);
+					canvas.setAttribute("height",position.h*ratio);
+					loadingboxhide();
+					if($("body").hasClass('init')){
+						$("body").removeClass('init');
+					}
+				}
+            }  
+        });  
+	})
+	var oX, oY, imghtml;
+	var isTouchStart = false, isTouchMove = false;
+	$(".emojitable div img").bind('touchstart',function(e){
+		isTouchStart = true;
+		isTouchMove = false;
+	})
+	$(".emojitable div img").bind('touchmove',function(e){
+		isTouchMove = true;
+	})
+	$(".emojitable div img").bind('touchend',function(e){
+		if(isTouchStart&&!isTouchMove){
+			$(".emoji").removeClass('adjusting');
+			var src = $(this).attr("src").replace("emoji64","emoji160");
+			// var src = $(this).attr("src").replace("emoji64","emoji160");
+			imghtml = "<img src='"+src+"' class='emoji adjusting' style='left:"+(position.w/2-40)+"px;top:"+(position.h/2-40)+"px;' data-size='1' data-rotate='0' />";
+			$(imghtml).appendTo('.imagebox');
+			loadingboxshow();
+			oX = $(this).offset().left;
+			oY = $(this).offset().top;
+			$("#emojibox").removeClass('show');
+			$(".ctrlbottom").addClass('ctrlstate');
+			$(".emoji.adjusting")[0].onload = function(){
+				$(".emoji.adjusting").beginAdjust();
+				loadingboxhide();
+			}
+		}
+	})
 
-$.fn.dragInit = function(){
-	var $this = $(this);
-	$(document).on('touchmove.init',function(e){
+	$.fn.dragInit = function(){
+
+	}
+	var emojioX, emojioY, emojitX, emojitY;
+
+	// 给所有emoji绑定函数
+	$(document).on('touchstart','.emoji',function(e){
 		e.preventDefault();e.stopPropagation();
-		var _oX = oX + e.originalEvent.changedTouches[0].pageX - tX,
-			_oY = oY + e.originalEvent.changedTouches[0].pageY - tY;
-		$this.css({"top":_oY,"left":_oX});
-		if(e.originalEvent.changedTouches[0].pageY>sW*_r){}
-		else{
-			hasEmojiOut = true;
+		emojioX = $(this).position().left;
+		emojioY = $(this).position().top;
+		emojitX = e.originalEvent.changedTouches[0].pageX;
+		emojitY = e.originalEvent.changedTouches[0].pageY;
+		if(!$(this).hasClass("adjusting")){
+			$(this).beginAdjust();//当前emoji开始调整
 		}
 	})
-	$(document).on('touchend.init',function(e){
-		$(this).off(".init");
-		$this.removeClass('draging').beginAdjust();
-		if(e.originalEvent.changedTouches[0].pageY>sW*_r){
-			$this.removeEmoji();
+	$(document).on('touchmove','.emoji',function(e){
+		e.preventDefault();e.stopPropagation();
+		var _oX = emojioX + e.originalEvent.changedTouches[0].pageX - emojitX,
+			_oY = emojioY + e.originalEvent.changedTouches[0].pageY - emojitY;
+		$(this).css({"top":_oY,"left":_oX});
+	})
+	$(document).on('touchend',".emoji",function(e){
+		e.preventDefault();e.stopPropagation();
+		if($(this).hasClass('draging')){
+			$(this).removeClass('draging');
 		}
 	})
-}
-var emojioX, emojioY, emojitX, emojitY;
+	
+	$.fn.beginAdjust = function(){
+		$(".emoji").not(this).removeClass('adjusting');
+		$(this).addClass('adjusting');
+		$("#ctrlbottom.ctrlbottom").addClass('ctrlstate');
+		var vs = $(this).attr("data-size"),
+		    vr = $(this).attr("data-rotate");
+		$("#sizerange").val(vs);
+		$("#rotaterange").val(vr);
+		$(this).bindCtrl();
+	}
+	$.fn.bindCtrl = function(){
+		var $this = $(this),w = $(this)[0].naturalHeight;
+		$('.range').unbind(".control");
+		$("#sizerange").bind('touchmove.control',function(){
+			var v = $(this).val();
+			$this.css({"width":w*v/2+"px"}).attr("data-size",v);
+		});
+		$("#rotaterange").val($this.attr("data-rotate")).bind('touchmove.control',function(){
+			var v = $(this).val();
+			$this.css({"transform":"rotate("+v+"deg)","-webkit-transform":"rotate("+v+"deg)"}).attr("data-rotate",v);
+		})
+	}
+	$.fn.removeEmoji = function(){
+		$(this).remove();
+		$("#ctrlbottom.ctrlbottom").removeClass('ctrlstate');
+	}
 
-// 给所有emoji绑定函数
-$(document).on('touchstart','.emoji',function(e){
-	e.preventDefault();e.stopPropagation();
-	emojioX = $(this).offset().left;
-	emojioY = $(this).offset().top;
-	emojitX = e.originalEvent.changedTouches[0].pageX;
-	emojitY = e.originalEvent.changedTouches[0].pageY;
-	saveAdjust();//保存之前的调整结果
-	$(this).beginAdjust();//当前emoji开始调整
-})
-$(document).on('touchmove','.emoji',function(e){
-	e.preventDefault();e.stopPropagation();
-	var _oX = emojioX + e.originalEvent.changedTouches[0].pageX - emojitX,
-		_oY = emojioY + e.originalEvent.changedTouches[0].pageY - emojitY;
-	$(this).css({"top":_oY,"left":_oX});
-	if(!($(this).hasClass('draging'))){
-		$(this).addClass('draging');
+	function sizereset(maxwidth,maxheight,width,height){
+	  var r = maxwidth/maxheight,
+	      radio = width/height,
+	      dx = dy = 0;
+	  if(radio >= r){
+	    width=maxwidth;
+	    height=maxwidth/radio;
+	    dy=(maxheight-height)/2;
+	  }
+	  else{
+	    height=maxheight;
+	    width=maxheight*radio;
+	    dx = (maxwidth-width)/2;
+	  }
+	  var position = {
+	    w:width,
+	    h:height,
+	    dx:dx,
+	    dy:dy
+	  }
+	  return position;
 	}
-	//切换removestate状态
-	if(e.originalEvent.changedTouches[0].pageY>sW*_r){
-		$("#beyond").addClass('removestate');
-	}
-	else{
-		$("#beyond").removeClass('removestate');
-		hasEmojiOut = true;
-	}
-})
-$(document).on('touchend',".emoji",function(e){
-	e.preventDefault();e.stopPropagation();
-	if($(this).hasClass('draging')){
-		$(this).removeClass('draging');
-	}
-	if($("#beyond").hasClass('removestate')){
-		$(this).removeEmoji();
-	}
-})
-var dx;
-$(document).on('touchstart',".longbox",function(e){
-	tX = e.originalEvent.changedTouches[0].pageX;
-	hasEmojiOut = false;
-})
-$(document).on('touchmove',".longbox",function(e){
-	dx = e.originalEvent.changedTouches[0].pageX - tX;
-})
-$(document).on('touchend',".longbox",function(e){
-	// 如果第一次点在了表情上且移出去过，那么禁止滑动
-	if(hasEmojiOut == false){
-		var p = $(this).attr("data-page")-0,
-		maxp = $(this).children(".emojitable").length;
-		if(p>=0 && p<maxp){
-			var l;
-			if(dx > 0 && p>0){
-				l = $(".emojitable[data-index='"+(--p)+"']",this).position().left;
-			}
-			else if(dx<0 && p<maxp-1){
-				l = $(".emojitable[data-index='"+(++p)+"']",this).position().left;
-			}
-			else{}
-			$(this).attr("data-page",p).closest(".scrollbox").stop(true).animate({scrollLeft:l}, 300, function(){
-				$(this).find('.pagenav a').removeClass('now').filter(":nth-child("+(p+1)+")").addClass('now');
-			});
+	function getUrl(file){
+		var url;
+		if (window.createObjectURL != undefined) {
+			url = window.createObjectURL(file)
+		} 
+		else if (window.URL != undefined) {
+			url = window.URL.createObjectURL(file)
+		} 
+		else if(window.webkitURL != undefined) {
+			url = window.webkitURL.createObjectURL(file)
 		}
+		return url;
 	}
-})
-$("input[type='range']").bind('touchmove',function(){
-	var v = $(this).val();
-	$(this).next('input[type="text"]').val(v);
-})
-$("#confirmbtn").bind('click',function(){
-	saveAdjust();
-	$("#beyond").removeClass('ctrlstate');
-})
-$("#resetbtn").bind('click',function(){
-	var w = $(".emoji.adjusting")[0].naturalHeight;
-	$(".emoji.adjusting").css({"width":w,"transform":"rotate(0)","-webkit-transform":"rotate(0)"}).attr({"data-size":1,"data-rotate":0});
-	$("#sizerange").val(1).next('input[type="text"]').val(1);
-	$("#rotaterange").val(0).next('input[type="text"]').val(0);
-})
-//选择section
-$(".section div").bind('touchend',function(){
-	var t = $(this).attr("data-tab");
-	$("#emojibox").attr("data-showid",t);
-})
-$.fn.beginAdjust = function(){
-	$("#beyond").addClass('ctrlstate');
-	var vs = $(this).attr("data-size")?$(this).attr("data-size"):1,
-	    vr = $(this).attr("data-rotate")?$(this).attr("data-rotate"):0;
-	$("#sizerange").val(vs).next('input[type="text"]').val(vs);
-	$("#rotaterange").val(vr).next('input[type="text"]').val(vr);
-	$(this).bindCtrl();
-}
-$.fn.bindCtrl = function(){
-	var $this = $(this),w = $(this)[0].naturalHeight;
-	$(".emoji").removeClass('adjusting');
-	$this.addClass('adjusting');
-	$('.range').unbind(".control");
-	$("#sizerange").bind('touchmove.control',function(){
-		var v = $(this).val()/2;
-		$this.css({"width":w*v});
-	});
-	$("#rotaterange").val($this.attr("data-rotate")).bind('touchmove.control',function(){
-		var v = $(this).val();
-		$this.css({"transform":"rotate("+v+"deg)","-webkit-transform":"rotate("+v+"deg)"});
+
+		$("#savebtn").bind("click",function(){
+			loadingboxshow();
+			var img = $(".imagebox img");
+			context.drawImage(img[0],0,0,img[0].naturalWidth,img[0].naturalHeight,0,0,ratio*position.w,ratio*position.h);
+			context.save();
+			$.each($(".emoji"),function(){
+				var emoji = $(this);
+				var angle = emoji.attr("data-rotate")*Math.PI/180;
+				var left = emoji.position().left,
+					top = emoji.position().top;
+			 	var dx;
+			 	if(angle<Math.PI/2&&angle>0||angle<-Math.PI/2){
+			 		dx = 0.70710678*Math.cos(Math.PI/4-angle)*emoji.outerWidth();
+			 	}
+			 	else{
+			 		dx = -0.70710678*Math.cos(angle+Math.PI/4)*emoji.outerWidth();
+			 	}
+			 	dx = dx < 0?-dx:dx;
+			 	var xpos = left+dx;
+			    var ypos = top+dx;
+				context.save();
+				context.translate(ratio*xpos, ratio*ypos);
+				context.rotate(angle);
+				context.translate(-ratio*xpos, -ratio*ypos);
+				context.drawImage(emoji[0], ratio*(xpos-0.5*emoji.width()), ratio*(ypos-0.5*emoji.height()), ratio*emoji.width(), ratio*emoji.height());
+				context.restore();
+			});
+			var image = getimage(canvas);
+			$(image).css({
+				"width":position.w,
+				"height":position.h
+			}).addClass("veryCenter").appendTo('#composeimagebox');
+			$(".savebox").show();
+		})
+		
+	function getPixelRatio(context){
+	    var backingStore = context.backingStorePixelRatio ||
+	          context.webkitBackingStorePixelRatio ||
+	          context.mozBackingStorePixelRatio ||
+	          context.msBackingStorePixelRatio ||
+	          context.oBackingStorePixelRatio ||
+	          context.backingStorePixelRatio || 1;
+	    return (window.devicePixelRatio || 1) / backingStore;
+	}
+	function getimage(canvas){
+		var image = new Image();
+	    image.src = canvas.toDataURL("image/png");
+	    $("#savebtn>a").attr("href",canvas.toDataURL("image/png"));
+	    return image;
+	}
+	function loadingboxshow(){
+		$(".loadingbox").show();
+	}
+	function loadingboxhide(){
+		$(".loadingbox").hide();
+	}
+	$(".addemojibtn").bind('touchend',function(){
+		$("#emojibox").toggleClass('show');
 	})
-}
-$.fn.removeEmoji = function(){
-	$(this).remove();
-	$("#beyond").removeClass('removestate ctrlstate');
-}
-function saveAdjust(){
-	$(".emoji.adjusting").attr({"data-size":$("#sizerange").next("input[type='text']").val(),"data-rotate":$("#rotaterange").next("input[type='text']").val()});
-}
-function sizereset(maxwidth,maxheight,width,height){
-	// alert(maxwidth+","+maxheight+","+width+","+height);
-  var r = maxwidth/maxheight,
-      radio = width/height,
-      dx = dy = 0;
-  if(radio >= r){
-    width=maxwidth;
-    height=maxwidth/radio;
-    dy=(maxheight-height)/2;
-  }
-  else{
-    height=maxheight;
-    width=maxheight*radio;
-    dx = (maxwidth-width)/2;
-  }
-  var position = {
-    w:width,
-    h:height,
-    dx:dx,
-    dy:dy
-  }
-  return position;
-}
-function getUrl(file){
-	var url;
-	if (window.createObjectURL != undefined) {
-		url = window.createObjectURL(file)
-	} 
-	else if (window.URL != undefined) {
-		url = window.URL.createObjectURL(file)
-	} 
-	else if(window.webkitURL != undefined) {
-		url = window.webkitURL.createObjectURL(file)
-	}
-	return url;
-}
+	$("#removebtn").bind('touchend',function(){
+		$(".emoji.adjusting").removeEmoji();
+	})
+	$("#resetbtn").bind('touchend',function(){
+		var w = "80px";
+		$(".emoji.adjusting").css({"width":w,"transform":"rotate(0)","-webkit-transform":"rotate(0)"}).attr({"data-size":1,"data-rotate":0});
+		$("#sizerange").val(1);
+		$("#rotaterange").val(0);
+	})
+	$("#returnbtn").bind('click',function(){
+		$(".savebox").hide();
+		$("#composeimagebox").empty();
+	})
+	$("#copybtn").bind('touchend',function(){
+		var ori = $(".emoji.adjusting"),
+		    clone = ori.clone();
+		ori.removeClass('adjusting');
+		clone.css({
+			"left":ori.position().left+10+"px",
+			"top" :ori.position().top+10+"px"
+		}).appendTo('.imagebox');
+		$("#ctrlbottom").removeClass('ctrlstate');
+	})
+	$("#indexbtn").bind('touchend',function(){
+		var ori = $(".emoji.adjusting");
+		if(!ori.is(':last-child')){
+			var next = ori.next(".emoji");
+			ori.insertAfter(next);
+		}
+	})
+	$("#firstnewimage").bind('touchend',function(){
+		$('#photofile').trigger('click');
+	})
+	$(".closebtn").bind('touchend',function(){
+		$(".appinfo").hide();
+	})
+	$(".info").bind('touchend',function(){
+		$(".appinfo").show();
+	})
+	var tX, dx;
+	$(document).on('touchstart',".longbox",function(e){
+		tX = e.originalEvent.changedTouches[0].pageX;
+	})
+	$(document).on('touchmove',".longbox",function(e){
+		dx = e.originalEvent.changedTouches[0].pageX - tX;
+	})
+	$(document).on('touchend',".longbox",function(e){
+			var p = $(this).attr("data-page")-0,
+			maxp = $(this).children(".emojitable").length;
+			if(p>=0 && p<maxp){
+				var l;
+				if(dx > 0 && p>0){
+					l = $(".emojitable[data-index='"+(--p)+"']",this).position().left;
+				}
+				else if(dx<0 && p<maxp-1){
+					l = $(".emojitable[data-index='"+(++p)+"']",this).position().left;
+				}
+				else{}
+				$(this).attr("data-page",p).closest(".scrollbox").stop(true).animate({scrollLeft:l}, 300, function(){
+					$(this).find('.pagenav a').removeClass('now').filter(":nth-child("+(p+1)+")").addClass('now');
+				});
+			}
+	})
+	//选择section
+	$(".section div").bind('touchend',function(){
+		var t = $(this).attr("data-tab");
+		$("#emojibox").attr("data-showid",t);
+	})
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-﻿## emoji表情大暴走
+## emoji表情大暴走
 
 emoji表情大暴走用于手机web拼接相册中的照片与emoji表情
 
 ## 支持retina显示屏
 
-拼接程序的核心代码使用canvas，并通过[hidip-canvas.js](https://github.com/jondavidjohn/hidpi-canvas-polyfill)(感谢Jonathan D. Johnson)使得canvas支持高清显示屏
+拼接程序的核心代码使用canvas，并通过[exif-js](https://github.com/exif-js/exif-js)获取手机相片的orientation，修正相片写入canvas时的翻转、压缩等问题
 
 ## 至简的设计理念
 
@@ -17,3 +17,4 @@ emoji表情大暴走用于手机web拼接相册中的照片与emoji表情
 ## 商业友好的开源协议
 
 emoji表情大暴走遵循Apache2开源协议发布。Apache Licence是著名的非盈利开源组织Apache采用的协议。该协议和BSD类似，鼓励代码共享和尊重原作者的著作权，同样允许代码修改，再作为开源或商业软件发布。
+
